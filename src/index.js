84b7e9bcb6a3a9500a0d0e911af09f046427026c
caroline sarre <anacarolinesarre@gmail.com>
2023-01-23 11:52:16 -0300

    adicionando export de componentes no index

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-export {button as Button} from './components/buttons'
+export { default as Button } from './components/buttons'
+export { default as Display } from './components/tipography/display'
+export { default as HeadingL } from './components/tipography/headingL'
+export { default as HeadingM } from './components/tipography/headingM'
+export { default as HeadingS } from './components/tipography/headingS'
+export { default as HeadingXs } from './components/tipography/headingXs'
+export { default as Subheading } from './components/tipography/subheading'
+export { default as BodyM } from './components/tipography/bodyM'
